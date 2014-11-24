e7eddfcb124e900b4e3e2ada7b7df2de84b28cc7
Griff Hall <griffhall1@gmail.com>
2014-11-24 11:53:04 -0600

    returns correct outcome

--- How Many Fingers/ViewController.swift
+++ How Many Fingers/ViewController.swift
@@ -14,7 +14,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var guess: UITextField!
 
+    @IBOutlet weak var message: UILabel!
+    @IBAction func guessButton(sender: AnyObject) {
+        
+        var numberOfFingers = arc4random() % 6
+        
+        println(numberOfFingers)
+        
+        var numberOfFingersString = String(numberOfFingers)
+        
+        if (numberOfFingersString==guess.text) {
+            
+            message.text = "You got it correct!"
+        }
+        else {
+            message.text = "Incorrect! I was holding up \(numberOfFingers) fingers"
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
